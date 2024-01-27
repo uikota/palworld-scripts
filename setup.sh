bdecc7f1a9a8561cf024ee8eeec897a57891b2dd
uikota <kotake545@gmail.com>
2024-01-28 00:32:10 +0900

    update setup.sh

--- setup.sh
+++ setup.sh
@@ -14,3 +14,30 @@ cd "${RCON_DIR}"
 curl -LO https://github.com/gorcon/rcon-cli/releases/download/v0.10.3/rcon-0.10.3-amd64_linux.tar.gz
 tar -xvzf rcon-0.10.3-amd64_linux.tar.gz --strip-components=1
 rm -f rcon-0.10.3-amd64_linux.tar.gz
+
+# systemctlに登録
+cat <<EOF > /etc/systemd/system/palworld.service
+[Unit]
+Description=palworld
+After=network-online.target
+
+[Service]
+Type=simple
+WorkingDirectory=${INSTALL_DIR}/
+LimitNOFILE=100000
+User=palworld
+
+ExecStartPre=/usr/games/steamcmd +login anonymous +app_update 2394010 validate +quit
+ExecStart=/bin/bash -c "./PalServer.sh"
+ExecStop=/bin/kill -s INT \$MAINPID
+
+Restart=on-failure
+RestartSec=30
+TimeoutSec=600
+
+[Install]
+WantedBy=multi-user.target
+EOF
+
+systemctl daemon-reload
+sudo systemctl enable palworld.service
