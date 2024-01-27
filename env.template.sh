a44d0385680a7d2ae095fed4d67af9da3942b550
uikota <kotake545@gmail.com>
2024-01-27 23:46:51 +0900

    first commit

---
+++ env.template.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+export HOME_DIR="/home/palworld"
+export RCON_DIR="${HOME_DIR}/rcon"
+export INSTALL_DIR="${HOME_DIR}/.steam/SteamApps/common/PalServer"
+export UPDATE_DIR="${HOME_DIR}/update/PalServer"
+export BACKUP_DIR="${HOME_DIR}/backup"
+export ADMIN_PASSWORD="ここにパスワード"
+
+
