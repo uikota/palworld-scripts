4f61550d4d7a1c0867c58d574bb4adf83c8ca794
uikota <kotake545@gmail.com>
2024-01-28 00:09:10 +0900

    update env.template.sh

--- env.template.sh
+++ env.template.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
 
-export HOME_DIR="/home/palworld"
+# スクリプト自身のフルパスを取得
+SCRIPT_PATH=$(readlink -f "$0")
+SCRIPT_DIR=$(dirname "$SCRIPT_PATH")
+
+# SCRIPT_DIRから一つ上のディレクトリを取得
+export HOME_DIR=$(dirname "$SCRIPT_DIR")
+
 export RCON_DIR="${HOME_DIR}/rcon"
 export INSTALL_DIR="${HOME_DIR}/.steam/SteamApps/common/PalServer"
 export UPDATE_DIR="${HOME_DIR}/update/PalServer"
 export BACKUP_DIR="${HOME_DIR}/backup"
 export ADMIN_PASSWORD="ここにパスワード"
 
-
+echo ${HOME_DIR}
