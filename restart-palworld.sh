55c64ddd4e5aa710b26c9922e6d9fd7fdd040f8a
uikota <kotake545@gmail.com>
2024-01-28 00:13:12 +0900

    add update-palworld.sh

---
+++ restart-palworld.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+SCRIPT_PATH=$(readlink -f "$0")
+SCRIPT_DIR=$(dirname "$SCRIPT_PATH")
+
+. "${SCRIPT_DIR}/env.sh"
+
+# サーバーに通知
+${RCON_DIR}/rcon -a "localhost:25575" -p ${ADMIN_PASSWORD} "Broadcast The_server_will_restart_after_5_minutes."
+
+# 5分待機
+sleep 300
+
+# サーバー再起動
+systemctl stop palworld.service
+systemctl start palworld.service
